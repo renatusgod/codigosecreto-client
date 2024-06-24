49cf95ab59c76e182a3ec22b8c2357f80299133f
Renato Miranda <miranda@faso.com.br>
2024-06-24 19:38:54 -0300

    to 18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthInterceptorService } from './auth/services/auth-interceptor.service';
@@ -8,25 +8,19 @@ import { AuthGuardService } from './auth/services/auth-guard.service';
 import { AuthService } from './auth/services/auth.service';
 import { LoginGuardService } from './auth/services/login-guard.service';
 
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-  ],
-  providers: [
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: AuthInterceptorService,
-      multi: true
-    },
-    AuthService,
-    AuthGuardService,
-    LoginGuardService
-  ],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule], providers: [
+        {
+            provide: HTTP_INTERCEPTORS,
+            useClass: AuthInterceptorService,
+            multi: true
+        },
+        AuthService,
+        AuthGuardService,
+        LoginGuardService,
+        provideHttpClient(withInterceptorsFromDi())
+    ] })
 export class AppModule { }
